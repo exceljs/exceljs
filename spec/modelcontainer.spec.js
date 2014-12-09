4a85419b68c2bf781c3f140e0ab6355ac9d38221
guyonroche <cyber.sapiens@hotmail.com>
2014-12-09 21:21:18 +0000

    Fixing version and test

--- spec/modelcontainer.spec.js
+++ spec/modelcontainer.spec.js
@@ -9,17 +9,13 @@ describe("ModelContainer", function() {
         var mcModel = JSON.parse(JSON.stringify(model));
         
         var mc = new Excel.ModelContainer(model);
-        //fs.writeFileSync("./testmodel1.json", JSON.stringify(model, null, "    "));
         mc.xlsx.writeFile("./mc.test.xlsx")
             .then(function() {
                 var mc2 = new Excel.ModelContainer();
                 return mc2.xlsx.readFile("./mc.test.xlsx");
             })
             .then(function(mc2) {
-                fs.writeFileSync("./testmodel2.json", JSON.stringify(mc2.model, null, "    "));
-                
                 expect(mc2.model).toEqual(mcModel);
-                //expect(_.isEqual(mc2.model,mc.model)).toBeTruthy();
             })
             .finally(function() {
                 fs.unlink("./mc.test.xlsx", function(error) {
