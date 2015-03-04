2550a8960b70443f4f3ffd930f678ad314b2d967
guyonroche <cyber.sapiens@hotmail.com>
2015-03-04 09:04:19 +0000

    Bumped up Archiver version

--- test/testUnzip.js
+++ test/testUnzip.js
@@ -10,28 +10,11 @@ fs.createReadStream(filename)
     .pipe(unzip.Parse())
     .on('entry',function (entry) {
         console.log(entry.path);
-        if (entry.path == "xl/sharedStrings.xml") {
-            ss = entry;
-        }
-    }).on('close', function () {
-        var parser = require("sax").createStream(true, {});
-        parser.on('opentag', function(node) {
-            var line = [];
-            line.push("<" + node.name);
-            //console.log(JSON.stringify(node.attributes));
-            _.each(node.attributes, function(value, name) {
-                line.push(name + '="' + value + '"');
-            });
-            console.log(line.join(" ") + ">");
-        });
-        parser.on('closetag', function (name) {
-            console.log("</" + name + ">");
-        });
-        parser.on('text', function (text) {
-            console.log(text);
-        });
-        parser.on('error', function (err) {
-            console.error(err);
-        });
-        ss.pipe(parser);
+    })
+    .on('close', function () {
+        console.log("Finished");
+    })
+    .on('error', function (error) {
+        console.log("Error: " + error.message);
     });
+    
