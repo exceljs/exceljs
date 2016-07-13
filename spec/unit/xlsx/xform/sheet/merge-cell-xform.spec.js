33807d87ac80dfe8ec081a29047c10fb7d7998e4
guyonroche <guyon@live.com>
2016-07-13 18:11:04 +0100

    more spec

---
+++ spec/unit/xlsx/xform/sheet/merge-cell-xform.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var MergeCellXform = require('../../../../../lib/xlsx/xform/sheet/merge-cell-xform');
+var testXformHelper = require('./../test-xform-helper');
+
+var expectations = [
+  {
+    title: 'Merge',
+    create:  function() { return new MergeCellXform()},
+    preparedModel: 'B2:C4',
+    xml: '<mergeCell ref="B2:C4"/>',
+    parsedModel: 'B2:C4',
+    tests: ['render', 'parse']
+  }
+];
+
+describe('MergeCellXform', function () {
+  testXformHelper(expectations);
+});
