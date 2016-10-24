89f5ae65f1b4320ad8512a16c742fd7a1caeff29
guyonroche <guyon@live.com>
2016-10-24 19:03:16 +0100

    Add integration test for issue 163

---
+++ spec/integration/issues.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+chai.use(require('chai-datetime'));
+
+var Excel = require('../../excel');
+
+// this file to contain integration tests created from github issues
+
+describe('github issues', function() {
+  it('issue 163 - Error while using xslx readFile method', function() {
+    var wb = new Excel.Workbook();
+    return wb.xlsx.readFile('./spec/integration/data/test-issue-163.xlsx')
+      .then(function() {
+        // arriving here is success
+        expect(true).to.equal(true);
+      });
+  });
+});
