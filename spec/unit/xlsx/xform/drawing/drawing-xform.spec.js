d62f29fb1b90b15e4dbd640c5fcc6e854eed9820
guyonroche <guyon@live.com>
2017-02-24 08:26:01 +0000

    Adding images

--- spec/unit/xlsx/xform/drawing/drawing-xform.spec.js
+++ spec/unit/xlsx/xform/drawing/drawing-xform.spec.js
@@ -8,10 +8,11 @@ var expectations = [
   {
     title: 'Drawing 1',
     create: function() { return new DrawingXform({tag: 'xdr:from'}); },
+    initialModel: require('./data/drawing.1.0.json'),
     preparedModel: require('./data/drawing.1.1.json'),
     xml: fs.readFileSync(__dirname + '/data/drawing.1.2.xml').toString(),
     parsedModel: require('./data/drawing.1.3.json'),
-    tests: ['render', 'renderIn', 'parse']
+    tests: ['prepare', 'render', 'renderIn', 'parse']
   }
 ];
 
