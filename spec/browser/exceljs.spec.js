bf55eea5a78a652700e84aef6c7330201d97564e
guyonroche <guyon@live.com>
2017-03-10 07:35:19 +0000

    Start of handling base64 stream output

--- spec/browser/exceljs.spec.js
+++ spec/browser/exceljs.spec.js
@@ -1,8 +1,5 @@
 'use strict';
-
-console.log('ereiamjh')
-require("babel-polyfill");
-
+require('babel-polyfill');
 var ExcelJS = require('../../lib/exceljs.browser');
 
 function unexpectedError(done) {
@@ -14,7 +11,7 @@ function unexpectedError(done) {
 }
 
 describe('ExcelJS', function() {
-  it('should read and write xlsx', function(done) {
+  it('should read and write xlsx via binary buffer', function(done) {
     var wb = new ExcelJS.Workbook();
     var ws = wb.addWorksheet('blort');
 
@@ -25,6 +22,34 @@ describe('ExcelJS', function() {
       .then(function(buffer) {
         var wb2 = new ExcelJS.Workbook();
         return wb2.xlsx.load(buffer)
+          .then(function() {
+            var ws2 = wb2.getWorksheet('blort');
+            expect(ws2).toBeTruthy();
+
+            expect(ws2.getCell('A1').value).toEqual('Hello, World!');
+            expect(ws2.getCell('A2').value).toEqual(7);
+            done();
+          });
+      })
+      .catch(function(error) {
+        throw error;
+      })
+      .catch(unexpectedError(done));
+  });
+  it('should read and write xlsx via base64 buffer', function(done) {
+    var options = {
+      base64: true
+    };
+    var wb = new ExcelJS.Workbook();
+    var ws = wb.addWorksheet('blort');
+
+    ws.getCell('A1').value = 'Hello, World!';
+    ws.getCell('A2').value = 7;
+
+    wb.xlsx.writeBuffer(options)
+      .then(function(buffer) {
+        var wb2 = new ExcelJS.Workbook();
+        return wb2.xlsx.load(buffer.toString('base64'), options)
           .then(function() {
             var ws2 = wb2.getWorksheet('blort');
             expect(ws2).toBeTruthy();
