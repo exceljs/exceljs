f264fdfdc6ec878574a456422a28676d11cac1e1
guyonroche <guyon@live.com>
2017-07-31 01:10:00 +0100

    Adding unit test

--- spec/unit/xlsx/xform/book/workbook-xform.spec.js
+++ spec/unit/xlsx/xform/book/workbook-xform.spec.js
@@ -13,7 +13,14 @@ var expectations = [
     xml: fs.readFileSync(__dirname + '/data/book.1.2.xml').toString().replace(/\r\n/g, '\n'),
     parsedModel: require('./data/book.1.3.json'),
     tests: ['render', 'renderIn', 'parse']
-  }
+  },
+  {
+    title: 'book.2 - no properties',
+    create: function() { return new WorkbookXform(); },
+    xml: fs.readFileSync(__dirname + '/data/book.2.2.xml').toString().replace(/\r\n/g, '\n'),
+    parsedModel: require('./data/book.2.3.json'),
+    tests: ['parse']
+  },
 ];
 
 describe('WorkbookXform', function() {
