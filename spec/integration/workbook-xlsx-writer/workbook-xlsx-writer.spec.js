33963d3605f7e45252782aa57618d63ed112eb60
Alexander James Phillips <ajp.global@gmail.com>
2017-12-15 17:28:11 +0000

    Setting column style does not default column width to 0

--- spec/integration/workbook-xlsx-writer/workbook-xlsx-writer.spec.js
+++ spec/integration/workbook-xlsx-writer/workbook-xlsx-writer.spec.js
@@ -125,7 +125,8 @@ describe('WorkbookWriter', function() {
       ws.columns = [
         { header: 'A1', width: 10 },
         { header: 'B1', style: colStyle },
-        { header: 'C1', width: 30 }
+        { header: 'C1', width: 30 },
+        { header: 'D1' }
       ];
 
       ws.getRow(2).font = testUtils.styles.fonts.broadwayRedOutline20;
@@ -158,6 +159,8 @@ describe('WorkbookWriter', function() {
           expect(ws2.getColumn(2).font).to.deep.equal(testUtils.styles.fonts.comicSansUdB16);
           expect(ws2.getColumn(2).alignment).to.deep.equal(testUtils.styles.namedAlignments.middleCentre);
           expect(ws2.getColumn(2).width).to.equal(undefined);
+
+          expect(ws2.getColumn(4).width).to.equal(undefined);
 
           expect(ws2.getRow(2).font).to.deep.equal(testUtils.styles.fonts.broadwayRedOutline20);
         });
