1ef6b6026b4eb80bbf4c8790e52638e91c3287ba
Andreas Lind <andreas.lind@peakon.com>
2018-04-23 16:41:58 +0200

    Accept an options object in XLSX.read(File)? and pass it down where it's needed

--- spec/integration/workbook-xlsx-reader.spec.js
+++ spec/integration/workbook-xlsx-reader.spec.js
@@ -2,6 +2,7 @@
 
 var verquire = require('../utils/verquire');
 var testutils = require('../utils/index');
+var fs = require('fs');
 var expect = require('chai').expect;
 
 var Excel = verquire('excel');
@@ -23,21 +24,43 @@ describe('WorkbookReader', function() {
     });
   });
 
-  describe('Row limit', function() {
-    it('should bail out if the file contains more rows than the limit', function() {
-      var workbook = new Excel.Workbook(10);
-      // The Fibonacci sheet has 19 rows
-      return workbook.xlsx.readFile('./spec/integration/data/fibonacci.xlsx')
-        .then(function() {
-          throw new Error('Promise unexpectedly fulfilled');
-        }, function(err) {
-          expect(err.message).to.equal('The number of rows exceeds the limit');
-        });
-    });
+  describe('#readFile', function() {
+    describe('Row limit', function() {
+      it('should bail out if the file contains more rows than the limit', function() {
+        var workbook = new Excel.Workbook();
+        // The Fibonacci sheet has 19 rows
+        return workbook.xlsx.readFile('./spec/integration/data/fibonacci.xlsx', {maxRows: 10})
+          .then(function() {
+            throw new Error('Promise unexpectedly fulfilled');
+          }, function(err) {
+            expect(err.message).to.equal('The number of rows exceeds the limit');
+          });
+      });
 
-    it('should parse fine if the limit is not exceeded', function() {
-      var workbook = new Excel.Workbook(20);
-      return workbook.xlsx.readFile('./spec/integration/data/fibonacci.xlsx');
+      it('should parse fine if the limit is not exceeded', function() {
+        var workbook = new Excel.Workbook();
+        return workbook.xlsx.readFile('./spec/integration/data/fibonacci.xlsx', {maxRows: 20});
+      });
+    });
+  });
+
+  describe('#read', function() {
+    describe('Row limit', function() {
+      it('should bail out if the file contains more rows than the limit', function() {
+        var workbook = new Excel.Workbook();
+        // The Fibonacci sheet has 19 rows
+        return workbook.xlsx.read(fs.createReadStream('./spec/integration/data/fibonacci.xlsx'), {maxRows: 10})
+          .then(function() {
+            throw new Error('Promise unexpectedly fulfilled');
+          }, function(err) {
+            expect(err.message).to.equal('The number of rows exceeds the limit');
+          });
+      });
+
+      it('should parse fine if the limit is not exceeded', function() {
+        var workbook = new Excel.Workbook();
+        return workbook.xlsx.read(fs.createReadStream('./spec/integration/data/fibonacci.xlsx'), {maxRows: 20});
+      });
     });
   });
 });
