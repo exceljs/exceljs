d66ea6c074f47ee120ff102369b1ac192060284f
Karl von Randow <karl@xk72.com>
2019-02-15 11:36:18 +1300

    Add test for one-cell-anchor and fix reconcile

--- spec/integration/workbook/images.spec.js
+++ spec/integration/workbook/images.spec.js
@@ -130,5 +130,46 @@ describe('Workbook', function() {
           expect(Buffer.compare(imageData, image.buffer)).to.equal(0);
         });
     });
+
+    it('stores embedded image with one-cell-anchor', function() {
+      var wb = new Excel.Workbook();
+      var ws = wb.addWorksheet('blort');
+      var wb2, ws2;
+
+      var imageId = wb.addImage({
+        filename: IMAGE_FILENAME,
+        extension: 'jpeg',
+      });
+
+      ws.addImage(imageId, {
+        tl: { col: 0.1125, row: 0.4 },
+        ext: { width: 100, height: 100 },
+        editAs: 'oneCell'
+      });
+
+      return wb.xlsx.writeFile(TEST_XLSX_FILE_NAME)
+        .then(function() {
+          wb2 = new Excel.Workbook();
+          return wb2.xlsx.readFile(TEST_XLSX_FILE_NAME);
+        })
+        .then(function() {
+          ws2 = wb2.getWorksheet('blort');
+          expect(ws2).to.not.be.undefined();
+
+          return fsReadFileAsync(IMAGE_FILENAME);
+        })
+        .then(function(imageData) {
+          const images = ws2.getImages();
+          expect(images.length).to.equal(1);
+
+          const imageDesc = images[0];
+          expect(imageDesc.range.editAs).to.equal('oneCell');
+          expect(imageDesc.range.ext.width).to.equal(100);
+          expect(imageDesc.range.ext.height).to.equal(100);
+
+          const image = wb2.getImage(imageDesc.imageId);
+          expect(Buffer.compare(imageData, image.buffer)).to.equal(0);
+        });
+    });
   });
 });
