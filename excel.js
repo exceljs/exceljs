635d86c639e9345bb160f09f0a63d5a16ad96456
guyonroche <guyon@live.com>
2019-04-02 09:15:00 +0100

    Remove superfluous copyright notices

--- excel.js
+++ excel.js
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2014-2017 Guyon Roche
- * LICENCE: MIT - please refer to LICENCE file included with this module
+ * Copyright (c) 2014-2019 Guyon Roche
+ * LICENCE: MIT - please refer to LICENSE file included with this module
  * or https://github.com/exceljs/exceljs/blob/master/LICENSE
  */
 
