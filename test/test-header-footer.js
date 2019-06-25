902d7e48593a0f5009aeaa3af4bcc7a15d8a12b9
Guyon <guyon@live.com>
2019-06-25 08:16:36 +0100

    Fix row height issue

---
+++ test/test-header-footer.js
@@ -0,0 +1,24 @@
+const Excel = require('../lib/exceljs.nodejs.js');
+const HrStopwatch = require('./utils/hr-stopwatch');
+
+const [, , filename] = process.argv;
+
+const wb = new Excel.Workbook();
+const ws = wb.addWorksheet('Foo');
+ws.getCell('B2').value = 'Hello, World!';
+
+ws.headerFooter.oddHeader = '&CHello, Header!';
+ws.headerFooter.oddFooter = '&CPage &P of &N';
+
+const stopwatch = new HrStopwatch();
+stopwatch.start();
+wb.xlsx
+  .writeFile(filename)
+  .then(() => {
+    const micros = stopwatch.microseconds;
+    console.log('Done.');
+    console.log('Time taken:', micros);
+  })
+  .catch(error => {
+    console.log(error.message);
+  });
