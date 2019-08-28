bd11bdd046f8cb5cc1c17069e38c0fff0faff389
egmen <egmen@mail.ru>
2019-08-28 16:47:33 +0300

    TS: Add types for addTable function
    closes #938

--- index.d.ts
+++ index.d.ts
@@ -1158,6 +1158,15 @@ export interface Worksheet {
 	 */
 	protect(password: string, options: Partial<WorksheetProtection>);
 	unprotect();
+
+	/**
+	 * Add a new table and return a reference to it
+	 */
+	addTable(tableProperties: TableProperties): Table;
+	/**
+	 * fetch table by name or id
+	 */
+	getTable(name: string): Table;
 }
 
 export interface WorksheetProperties {
@@ -1457,6 +1466,129 @@ export class Workbook {
 	addImage(img: Image): number;
 
 	getImage(id: number): Image;
+}
+
+export interface TableStyleProperties {
+	/**
+	 * The colour theme of the table
+	 * @default 'TableStyleMedium2'
+	 */
+	theme?: 'TableStyleDark1' | 'TableStyleDark10' | 'TableStyleDark11' | 'TableStyleDark2' | 'TableStyleDark3' | 'TableStyleDark4' | 'TableStyleDark5' | 'TableStyleDark6' | 'TableStyleDark7' | 'TableStyleDark8' | 'TableStyleDark9' | 'TableStyleLight1' | 'TableStyleLight10' | 'TableStyleLight11' | 'TableStyleLight12' | 'TableStyleLight13' | 'TableStyleLight14' | 'TableStyleLight15' | 'TableStyleLight16' | 'TableStyleLight17' | 'TableStyleLight18' | 'TableStyleLight19' | 'TableStyleLight2' | 'TableStyleLight20' | 'TableStyleLight21' | 'TableStyleLight3' | 'TableStyleLight4' | 'TableStyleLight5' | 'TableStyleLight6' | 'TableStyleLight7' | 'TableStyleLight8' | 'TableStyleLight9' | 'TableStyleMedium1' | 'TableStyleMedium10' | 'TableStyleMedium11' | 'TableStyleMedium12' | 'TableStyleMedium13' | 'TableStyleMedium14' | 'TableStyleMedium15' | 'TableStyleMedium16' | 'TableStyleMedium17' | 'TableStyleMedium18' | 'TableStyleMedium19' | 'TableStyleMedium2' | 'TableStyleMedium20' | 'TableStyleMedium21' | 'TableStyleMedium22' | 'TableStyleMedium23' | 'TableStyleMedium24' | 'TableStyleMedium25' | 'TableStyleMedium26' | 'TableStyleMedium27' | 'TableStyleMedium28' | 'TableStyleMedium3' | 'TableStyleMedium4' | 'TableStyleMedium5' | 'TableStyleMedium6' | 'TableStyleMedium7' | 'TableStyleMedium8' | 'TableStyleMedium9';
+	/**
+	  * Highlight the first column (bold)
+	  * @default false
+	  */
+	showFirstColumn?: boolean;
+	/**
+	  * Highlight the last column (bold)
+	  * @default false
+	  */
+	showLastColumn?: boolean;
+	/**
+	  * Alternate rows shown with background colour
+	  * @default false
+	  */
+	showRowStripes?: boolean;
+	/**
+	  * Alternate rows shown with background colour
+	  * @default false
+	  */
+	showColumnStripes?: boolean;
+}
+
+export interface TableColumnProperties {
+	/**
+	  * The name of the column, also used in the header
+	  */
+	name: string;
+	/**
+	  * Switches the filter control in the header
+	  * @default false
+	  */
+	filterButton?: boolean;
+	/**
+	  * Label to describe the totals row (first column)
+	  * @default 'Total'
+	  */
+	totalsRowLabel?: string;
+	/**
+	  * Name of the totals function
+	  * @default 'none'
+	  */
+	totalsRowFunction?: 'none' | 'average' | 'countNums' | 'count' | 'max' | 'min' | 'stdDev' | 'var' | 'sum' | 'custom';
+	/**
+	  * Optional formula for custom functions
+	  */
+	totalsRowFormula?: string;
+}
+
+
+export interface TableProperties {
+	/**
+	 * The name of the table
+	 */
+	name: string;
+	/**
+	 * The display name of the table
+	 */
+	displayName?: string;
+	/**
+	 * Top left cell of the table
+	 */
+	ref: string;
+	/**
+	 * Show headers at top of table
+	 * @default true
+	 */
+	headerRow?: boolean;
+	/**
+	 * Show totals at bottom of table
+	 * @default false
+	 */
+	totalsRow?: boolean;
+	/**
+	 * Extra style properties
+	 * @default {}
+	 */
+	style?: TableStyleProperties;
+	/**
+	 * Column definitions
+	 */
+	columns: TableColumnProperties[]
+	/**
+	 * Rows of data
+	 */
+	rows: any[][]
+}
+
+export type TableColumn = Required<TableColumnProperties>
+
+export interface Table extends Required<TableProperties> {
+	/**
+	 * Commit changes
+	 */
+	commit: () => void
+	/**
+	 * Remove a rows of data
+	 */
+	removeRows: (rowIndex: number, count: number) => void
+	/**
+	 * Add a row of data, either insert at rowNumber or append
+	 */
+	addRow: (values: any[], rowNumber: number) => void
+	/**
+	 * Get column
+	 */
+	getColumn: (colIndex: number) => TableColumn
+	/**
+	 * Add a new column, including column defn and values
+	 * inserts at colNumber or adds to the right
+	 */
+	addColumn: (column: TableColumnProperties, values: any[], colIndex: number) => void
+	/**
+	 * Remove a column with data
+	 */
+	removeColumns: (colIndex: number, count: number) => void
 }
 
 export namespace config {
