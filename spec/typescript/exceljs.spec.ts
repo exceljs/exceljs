fb0bfa83359e558aaece5bd06b0b9faa552b82a3
Guyon <guyon@live.com>
2020-01-27 18:25:54 +0000

    small ts spec refactor

--- spec/typescript/exceljs.spec.ts
+++ spec/typescript/exceljs.spec.ts
@@ -1,12 +1,11 @@
 import 'regenerator-runtime/runtime';
 
 import { expect } from 'chai';
-import * as ExcelJS from '../../excel';
-import * as types from '../../index';
+import ExcelJS from '../../index';
 
 describe('typescript', () => {
   it('can create and buffer xlsx', async () => {
-    const wb = new ExcelJS.Workbook() as types.Workbook;
+    const wb = new ExcelJS.Workbook();
     const ws = wb.addWorksheet('blort');
     ws.getCell('A1').value = 7;
     const buffer = await wb.xlsx.writeBuffer({
@@ -14,17 +13,17 @@ describe('typescript', () => {
       useSharedStrings: true,
     });
 
-    const wb2 = new ExcelJS.Workbook() as types.Workbook;
+    const wb2 = new ExcelJS.Workbook();
     await wb2.xlsx.load(buffer);
     const ws2 = wb2.getWorksheet('blort');
     expect(ws2.getCell('A1').value).to.equal(7);
   });
   it('can create and stream xlsx', async () => {
-    const wb = new ExcelJS.Workbook() as types.Workbook;
+    const wb = new ExcelJS.Workbook();
     const ws = wb.addWorksheet('blort');
     ws.getCell('A1').value = 7;
 
-    const wb2 = new ExcelJS.Workbook() as types.Workbook;
+    const wb2 = new ExcelJS.Workbook();
     const stream = wb2.xlsx.createInputStream();
     await wb.xlsx.write(stream);
     stream.end();
