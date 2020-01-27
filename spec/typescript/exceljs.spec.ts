fccedf997e4edaf8c18211e06fd7ebfeddc8bf0b
Guyon <guyon@live.com>
2020-01-27 18:09:37 +0000

    minor edits

--- spec/typescript/exceljs.spec.ts
+++ spec/typescript/exceljs.spec.ts
@@ -14,7 +14,7 @@ describe('typescript', () => {
       useSharedStrings: true,
     });
 
-    const wb2 = new ExcelJS.Workbook();
+    const wb2 = new ExcelJS.Workbook() as types.Workbook;
     await wb2.xlsx.load(buffer);
     const ws2 = wb2.getWorksheet('blort');
     expect(ws2.getCell('A1').value).to.equal(7);
