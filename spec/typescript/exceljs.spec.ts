ed16f74c850e53b1bf38f90de29d0ddac6cdbd97
Guyon <guyon@live.com>
2020-01-27 08:14:46 +0000

    Adding typescript tests to check the types file

---
+++ spec/typescript/exceljs.spec.ts
@@ -0,0 +1,41 @@
+import 'regenerator-runtime/runtime';
+
+import { expect } from 'chai';
+import * as ExcelJS from '../../excel';
+import * as types from '../../index';
+
+describe('typescript', () => {
+  it('can create and buffer xlsx', async () => {
+    const wb = new ExcelJS.Workbook() as types.Workbook;
+    const ws = wb.addWorksheet('blort');
+    ws.getCell('A1').value = 7;
+    const buffer = await wb.xlsx.writeBuffer({
+      useStyles: true,
+      useSharedStrings: true,
+    });
+
+    const wb2 = new ExcelJS.Workbook();
+    await wb2.xlsx.load(buffer);
+    const ws2 = wb2.getWorksheet('blort');
+    expect(ws2.getCell('A1').value).to.equal(7);
+  });
+  it('can create and stream xlsx', async () => {
+    const wb = new ExcelJS.Workbook() as types.Workbook;
+    const ws = wb.addWorksheet('blort');
+    ws.getCell('A1').value = 7;
+
+    const wb2 = new ExcelJS.Workbook() as types.Workbook;
+    const stream = wb2.xlsx.createInputStream();
+    await wb.xlsx.write(stream);
+    stream.end();
+
+    await new Promise((resolve, reject) => {
+      stream.on('done', () => {
+        const ws2 = wb2.getWorksheet('blort');
+        expect(ws2.getCell('A1').value).to.equal(7);
+        resolve();
+      });
+      stream.on('error', reject);
+    })
+  });
+});
