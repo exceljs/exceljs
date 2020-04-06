eb77600a1388246fadce2e21e5dcf1f91f69744b
mphender <matthew.p.henderson@nasa.gov>
2020-04-06 10:55:49 -0400

    changes by request: handle weird spinCount values, and provide test

---
+++ test/test-protection-spinCount.js
@@ -0,0 +1,49 @@
+const Excel = require('../lib/exceljs.nodejs.js');
+const HrStopwatch = require('./utils/hr-stopwatch');
+
+const [, , filename, password] = process.argv;
+
+const wb = new Excel.Workbook();
+const ws = wb.addWorksheet('Foo');
+ws.getCell('A1').value = 'Bar';
+
+async function save() {
+  const stopwatch = new HrStopwatch();
+
+  stopwatch.start();
+  await ws.protect(password); // default 100000
+  console.log('Protection Time [spinCount default]:', stopwatch.microseconds);
+
+  await wb.xlsx.writeFile(`${0}-${filename}`);
+
+  const values = [
+    100000,
+    10000,
+    1,
+    0,
+    -1,
+    undefined,
+    null,
+    NaN,
+    Infinity,
+    -Infinity,
+    31415.9265,
+  ];
+
+  for (let index = 1; index <= values.length; index += 1) {
+    const value = values[index - 1];
+
+    stopwatch.start();
+    await ws.protect(password, {spinCount: value});
+    console.log(
+      `Protection Time [spinCount ${value}]:`,
+      stopwatch.microseconds
+    );
+
+    await wb.xlsx.writeFile(`${index}-${filename}`);
+  }
+}
+
+save().catch(error => {
+  console.log(error.message);
+});
