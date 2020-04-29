fca10b6a882c9690b408cdbaf35f75194b284268
Alanscut <wp_scut@163.com>
2020-04-29 14:13:57 +0800

    update testcase

--- spec/integration/pr/test-pr-1220.spec.js
+++ spec/integration/pr/test-pr-1220.spec.js
@@ -1,7 +1,7 @@
 const ExcelJS = verquire('exceljs');
 
 describe('github issues', () => {
-  it('pull request 1216 - The worksheet should not be undefined', async () => {
+  it('pull request 1220 - The worksheet should not be undefined', async () => {
     const wb = new ExcelJS.Workbook();
     await wb.xlsx.readFile('./spec/integration/data/test-pr-1216.xlsx');
     const ws = wb.getWorksheet(1);
