98b4399d9477a6bcfd61336e5286d6caa7b97ee0
Subhajit Das <Subhajitdas298@gmail.com>
2020-06-04 06:52:58 +0530

    Changed babel compact to false

--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
     babel: {
       options: {
         sourceMap: true,
-        compact: true,
+        compact: false,
       },
       dist: {
         files: [
