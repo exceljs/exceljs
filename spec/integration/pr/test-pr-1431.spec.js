ad7317996795251fe5da3590b5bcdd0a80b44ffe
Reza Heidari <rheidari@distributorcentral.com>
2020-08-19 10:04:14 -0500

    added failing test for 1431, and fix for streaming workbook reader to emit rich text objects

---
+++ spec/integration/pr/test-pr-1431.spec.js
@@ -0,0 +1,43 @@
+const ExcelJS = verquire('exceljs');
+
+describe('github issues', () => {
+  it('pull request 1431 - streaming reader should handle rich text within shared strings', async () => {
+    const rowData = [
+      {
+        richText: [
+          {font: {bold: true}, text: 'This should '},
+          {font: {italic: true}, text: 'be one shared string value'},
+        ],
+      },
+      'this should be the second shared string',
+    ];
+
+    const workbook = new ExcelJS.stream.xlsx.WorkbookWriter({
+      filename: './test.xlsx',
+      useSharedStrings: true,
+    });
+
+    const sheet = workbook.addWorksheet('data');
+
+    sheet.addRow(rowData);
+
+    await workbook.commit();
+
+    const workbookReader = new ExcelJS.stream.xlsx.WorkbookReader('./test.xlsx', {
+      entries: 'emit',
+      hyperlinks: 'cache',
+      sharedStrings: 'cache',
+      styles: 'cache',
+      worksheets: 'emit',
+    });
+
+    for await (const worksheetReader of workbookReader) {
+      for await (const row of worksheetReader) {
+        // actual: 'This Should '
+        expect(row.values[1]).to.eql(rowData[0]);
+        // actual: 'be one shared string value'
+        expect(row.values[2]).to.equal(rowData[1]);
+      }
+    }
+  });
+});
