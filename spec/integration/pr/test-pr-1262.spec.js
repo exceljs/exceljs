fb59f285db52eb08af7f8d6a93f95d19b9d08b28
Reza Heidari <rheidari@distributorcentral.com>
2020-08-19 09:33:14 -0500

    updated test to read in file and ensure sheet protection is still there.

--- spec/integration/pr/test-pr-1262.spec.js
+++ spec/integration/pr/test-pr-1262.spec.js
@@ -1,7 +1,7 @@
 const ExcelJS = verquire('exceljs');
 
 describe('github issues', () => {
-  it('pull request 1262 - protect should not be undefined', async () => {
+  it('pull request 1262 - protect should work with streaming workbook writer', async () => {
     const workbook = new ExcelJS.stream.xlsx.WorkbookWriter({
       filename: './test.xlsx',
     });
@@ -13,5 +13,18 @@ describe('github issues', () => {
     };
 
     expect(sheet.protect).to.exist();
+
+    sheet.protect('password', {
+      spinCount: 1,
+    });
+
+    await workbook.commit();
+
+    // read in file and ensure sheetProtection is there:
+    const checkBook = new ExcelJS.Workbook();
+    await checkBook.xlsx.readFile('./test.xlsx');
+
+    const checkSheet = checkBook.getWorksheet('data');
+    expect(checkSheet.sheetProtection.spinCount).to.equal(1);
   });
 });
