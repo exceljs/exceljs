47969f624067dfd9ac2bebf66d3c4502171152c1
Kevin Kwok <kkwok@faralloncapital.com>
2020-09-28 18:40:45 -0700

    support reading names for streaming xlsx reads

---
+++ spec/integration/issues/issue-1364-incorrect-worksheet-name-streaming-xlsx.spec.js
@@ -0,0 +1,14 @@
+const ExcelJS = verquire('exceljs');
+
+// this file to contain integration tests created from github issues
+const TEST_XLSX_FILE_NAME = './spec/integration/data/test-issue-1364.xlsx';
+
+describe('github issues', () => {
+  it('issue 1364 - Incorrect Worksheet Name on Streaming XLSX Reader', async () => {
+    const workbookReader = new ExcelJS.stream.xlsx.WorkbookReader(TEST_XLSX_FILE_NAME, {});
+
+    for await (const worksheetReader of workbookReader) {
+      expect(worksheetReader.name).to.equal('Sum Worksheet');
+    }
+  });
+});
