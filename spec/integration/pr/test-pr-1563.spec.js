4656794443e37129a041dc1cce389f9e9b5ed58f
Teodor Marian <teodor_marian@external.mckinsey.com>
2020-12-15 15:42:18 +0200

    fix unit test

--- spec/integration/pr/test-pr-1563.spec.js
+++ spec/integration/pr/test-pr-1563.spec.js
@@ -5,7 +5,7 @@ describe('github issues', () => {
     it('should parse correctly', () => {
       const wb = new ExcelJS.Workbook();
       return wb.xlsx
-        .readFile('./spec/integration/data/test-pr-9999.xlsx')
+        .readFile('./spec/integration/data/test-pr-1563.xlsx')
         .then(() => {
           expect(wb.worksheets[0].columns[0].width).to.equal(23.6640625);
           expect(wb.worksheets[0].columns[5].hidden).to.equal(false);
