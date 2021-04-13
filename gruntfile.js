7cb6bcb5449c57eb95c120f4e03e31b96b5b2d38
Lauri Svan <lauri.svan@specsign.com>
2021-04-13 13:57:36 +0300

    Remove 'use strict' strings in brute force way from the builds to enable module packaging

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,7 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-babel');
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-string-replace');
   grunt.loadNpmTasks('grunt-terser');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-copy');
@@ -27,14 +28,17 @@ module.exports = function(grunt) {
     browserify: {
       options: {
         transform: [
-          ['babelify', {
-            // enable babel transpile for node_modules
-            global: true,
-            presets: ['@babel/preset-env'],
-            // core-js should not be transpiled
-            // See https://github.com/zloirock/core-js/issues/514
-            ignore: [/node_modules[\\/]core-js/],
-          }],
+          [
+            'babelify',
+            {
+              // enable babel transpile for node_modules
+              global: true,
+              presets: ['@babel/preset-env'],
+              // core-js should not be transpiled
+              // See https://github.com/zloirock/core-js/issues/514
+              ignore: [/node_modules[\\/]core-js/],
+            },
+          ],
         ],
         browserifyOptions: {
           // enable source map for browserify
@@ -50,7 +54,7 @@ module.exports = function(grunt) {
       bundle: {
         // keep the original source for source maps
         src: ['./lib/exceljs.browser.js'],
-        dest: './dist/exceljs.js',
+        dest: './build/web/exceljs.browserify.js',
       },
       spec: {
         options: {
@@ -59,6 +63,21 @@ module.exports = function(grunt) {
         },
         src: ['./build/spec/browser/exceljs.spec.js'],
         dest: './build/web/exceljs.spec.js',
+      },
+    },
+
+    'string-replace': {
+      dist: {
+        options: {
+          replacements: [
+            {
+              pattern: /("use strict")|('use strict')/g,
+              replacement: '\'\'',
+            },
+          ],
+        },
+        src: ['./build/web/exceljs.browserify.js'],
+        dest: './dist/exceljs.js',
       },
     },
 
@@ -128,6 +147,6 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.registerTask('build', ['babel:dist', 'browserify', 'terser', 'exorcise', 'copy']);
+  grunt.registerTask('build', ['babel:dist', 'browserify', 'string-replace', 'terser', 'exorcise', 'copy']);
   grunt.registerTask('ug', ['terser']);
 };
