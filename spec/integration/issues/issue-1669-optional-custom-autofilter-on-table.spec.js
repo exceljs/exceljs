596ce33b7326335a5b7f3c706ad8b5ec8e114e29
Todd Hambley <thambley@tlcorporate.com>
2021-05-12 16:24:07 -0400

    filters may be customFilters or just filters

--- spec/integration/issues/issue-1669-optional-custom-autofilter-on-table.spec.js
+++ spec/integration/issues/issue-1669-optional-custom-autofilter-on-table.spec.js
@@ -1,7 +1,7 @@
 const ExcelJS = verquire('exceljs');
 
 describe('github issues', () => {
-  it('issue 1669 - optional custom autofilter on table', () => {
+  it('issue 1669 - optional autofilter and custom autofilter on tables', () => {
     const wb = new ExcelJS.Workbook();
     return wb.xlsx.readFile('./spec/integration/data/test-issue-1669.xlsx');
   }).timeout(6000);
